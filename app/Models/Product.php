aefffa302963caac96e6a8cd19cdf1d287d93a39
nguyendungnd204 <ngvandungnvd204@gmail.com>
2025-07-06 21:18:43 +0700

    add model category and product

---
+++ app/Models/Product.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'quantity',
+        'category_id',
+        'price',
+        'image',
+        'status'
+    ];
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+}
